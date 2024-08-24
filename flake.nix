8c9c277010007027191f3681b9e06d2e50c74944
Sergii Maksymov <ua.maximoff@gmail.com>
2024-08-24 12:03:01 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Zed editor from Zed Industries, creators of Atom editor.";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        overlayAttrs = {
+            inherit (config.packages) zed-editor;
+        };
+        packages = rec {
+            zed-editor = pkgs.callPackage ./package {};
+            default = zed-editor;
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
